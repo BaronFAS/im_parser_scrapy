ba65381ab7c3ea7228016d64ff75ba8888f8f54e
priselkov <priselkov@gardium.ru>
2023-11-10 15:34:02 +0300

    Fix name variable

--- im_parser/utils.py
+++ im_parser/utils.py
@@ -1,15 +1,17 @@
 ﻿import re
 
+from im_parser.constants import RE_TITLE, SPICE, EMPTY_STRING
+
 
 def title_split_string(product_title):
-    match = re.search(r"(.*?)(\d+[.,]?\d*?\s*(?:мл|гр))(.*)", product_title)
+    match = re.search(RE_TITLE, product_title)
     if match:
         title_name, volume_product = (
-            match.group(1).strip() + " " + match.group(3).strip().rstrip(),
+            match.group(1).strip() + SPICE + match.group(3).strip().rstrip(),
             match.group(2).strip(),
         )
         title_name = title_name.rstrip()
-        volume_product = volume_product or ""
+        volume_product = volume_product or EMPTY_STRING
         return title_name, volume_product
     else:
         return product_title, None
