a86acd64f73a24e8062c43d1da04239838f9cb9b
priselkov <priselkov@gardium.ru>
2023-11-10 15:32:29 +0300

    Add new data param

--- im_parser/spiders/maksavit.py
+++ im_parser/spiders/maksavit.py
@@ -4,7 +4,8 @@ from typing import Dict
 from datetime import datetime as dt
 from im_parser.constants import (
     DOMAIN_NAME, START_URL, RU, XPATH_PAGE_URL, GET_PART_URL, RE_GET_NUMBER,
-    XPATH_TITLE,
+    XPATH_TITLE, XPATH_MARKETING_TAG, XPATH_BRAND, XPATH_SECTION,
+    SECTION_ONLY,
 )
 from im_parser.utils import title_split_string
 
@@ -28,10 +29,22 @@ class MaksavitSpider(scrapy.Spider):
         product_id = re.search(RE_GET_NUMBER, response.url)
         product_title = response.xpath(XPATH_TITLE).extract()[0]
         title_name, volume_product = title_split_string(product_title)
+        marketing_tags = response.xpath(XPATH_MARKETING_TAG).extract()
+        marketing_tags = [tag.strip() for tag in marketing_tags]
+        brand = response.xpath(XPATH_BRAND).extract_first()
+        if brand:
+            brand = brand.strip()
+        section = response.xpath(XPATH_SECTION).extract()
+        section = [tag.strip() for tag in section]
+        price_data = ''
         data = {
             'timestamp': dt.now().timestamp(),
             'RPC': product_id.group(),
             'url': response.url,
-            'title': f'{title_name}, {volume_product}'
+            'title': f'{title_name}, {volume_product}',
+            'marketing_tags': marketing_tags,
+            'brand': brand,
+            'section': section[:SECTION_ONLY],
+            'price_data': price_data,
         }
         yield data
