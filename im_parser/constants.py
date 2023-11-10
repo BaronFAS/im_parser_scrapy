184cf38752418f84d6dee0e3280b2a073bd9608a
priselkov <priselkov@gardium.ru>
2023-11-10 14:05:34 +0300

    Add start settings

---
+++ im_parser/constants.py
@@ -0,0 +1,15 @@
+﻿# spider общие настройки
+DOMAIN_NAME = 'maksavit'
+RU = '.ru'
+START_URL = 'https://' + DOMAIN_NAME + RU + '/novosibirsk/catalog/materinstvo_i_detstvo/detskaya_gigiena/' # noqa
+# не забыть добавить
+START_PAGE_INDEX = 1
+
+# Парсинг страницы каталога
+XPATH_PAGE_URL = '//a[@class="product-card-block__title"]/@href'
+GET_PART_URL = '?page='
+
+# Парсинг страницы продукта
+RE_GET_NUMBER = r'\d+'
+# Регулярное выражение для выбора числа из строки
+XPATH_TITLE = '//h1[@class="product-top__title"]/text()'
---
+++ im_parser/utils.py
@@ -0,0 +1,15 @@
+﻿import re
+
+
+def title_split_string(product_title):
+    match = re.search(r"(.*?)(\d+[.,]?\d*?\s*(?:мл|гр))(.*)", product_title)
+    if match:
+        title_name, volume_product = (
+            match.group(1).strip() + " " + match.group(3).strip().rstrip(),
+            match.group(2).strip(),
+        )
+        title_name = title_name.rstrip()
+        volume_product = volume_product or ""
+        return title_name, volume_product
+    else:
+        return product_title, None
