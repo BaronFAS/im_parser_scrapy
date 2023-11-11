d8199e8d88f2cba3ff86681eb5ad1f30881c06a5
priselkov <priselkov@gardium.ru>
2023-11-11 18:53:19 +0300

    Add class ScrapyItem

--- im_parser/items.py
+++ im_parser/items.py
@@ -1,12 +1,17 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class ImParserItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ScrapyItem(scrapy.Item):
+    """Определить данные, извлекаемые при парсинге."""
+    timestamp = scrapy.Field()
+    RPC = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    marketing_tags = scrapy.Field()
+    brand = scrapy.Field()
+    section = scrapy.Field()
+    price_data = scrapy.Field()
+    stock = scrapy.Field()
+    assets = scrapy.Field()
+    metadata = scrapy.Field()
+    variants = scrapy.Field()
