e2d58d560f93356e0463c133d01fb52dba6a4f93
priselkov <priselkov@gardium.ru>
2023-11-11 13:48:14 +0300

    Add new constants

--- im_parser/constants.py
+++ im_parser/constants.py
@@ -1,9 +1,16 @@
 ﻿# spider общие настройки
 DOMAIN_NAME = 'maksavit'
 RU = '.ru'
-START_URL = 'https://' + DOMAIN_NAME + RU + '/novosibirsk/catalog/materinstvo_i_detstvo/detskaya_gigiena/' # noqa
+START_URL = (
+    'https://'
+    + DOMAIN_NAME
+    + RU
+    + '/novosibirsk/catalog/materinstvo_i_detstvo/detskaya_gigiena/'
+)
 # не забыть добавить
 START_PAGE_INDEX = 1
+SPICE = ' '
+EMPTY_STRING = ''
 
 
 # Парсинг страницы каталога
@@ -13,39 +20,72 @@ GET_PART_URL = '?page='
 
 # Парсинг страницы продукта
 XPATH_TITLE = '//h1[@class="product-top__title"]/text()'
-XPATH_MARKETING_TAG = '//div[contains(@class, "product-picture")]//div[contains(@class, "badge-discount")]/text()' # noqa
+XPATH_MARKETING_TAG = '//div[contains(@class, "product-picture")]//div[contains(@class, "badge-discount")]/text()'  # noqa
 XPATH_BRAND = '//a[contains(@class, "product-info__brand-value")]/text()'
-XPATH_SECTION = '//ul[contains(@class, "breadcrumbs")]//span[@itemprop="name"]/text()' # noqa
-XPATH_PRICE_CURRENT = '//div[@class="price-box"]//span[@class="price-value"]/text()' # noqa
-XPATH_PRICE_ORIGINAL = '//div[@class="price-box"]//div[@class="price-box__old-price"]/text()' # noqa
+XPATH_SECTION = (
+    '//ul[contains(@class, "breadcrumbs")]//span[@itemprop="name"]/text()'
+)
+XPATH_PRICE_CURRENT = (
+    '//div[@class="price-box"]//span[@class="price-value"]/text()'
+)
+XPATH_PRICE_ORIGINAL = (
+    '//div[@class="price-box"]//div[@class="price-box__old-price"]/text()'
+)
 XPATH_IN_STOCK = '//div[@class="available-count"]'
 XPATH_MAIN_IMAGE = '//div[@class="product-picture"]//img/@src'
 XPATH_METADATA = '//div[contains(@class, "product-instruction__guide")]'
 XPATH_METADATA_H3 = './/h3[@class="desc"]'
 XPATH_TEXT = './text()'
-XPATH_SIBLING_TEXT = 'following-sibling::text()[1]'
+XPATH_SIB_TEXT = "following-sibling::text()[1]"
 XPATH_DEC_KEY = '//div[contains(@class, "product-instruction")]//h2/text()'
 XPATH_DEC_VALUE = '//div[contains(@class, "product-instruction")]//div/text()'
 
+# Ключи для словаря data
+BRAND = 'brand'
+ORIGINAL = 'original'
+CURRENT = 'current'
+DECSRIPTION_KEY = 'description_key'
+DECSRIPTION_VALUE = 'description_value'
+MARKETING_TAGS = 'marketing_tags'
+SECTION = 'section'
+TIMESTAMP = 'timestamp'
+RPC = 'RPC'
+URL = 'url'
+TITLE = 'title'
+PRICE_DATA = 'price_data'
+SALE_TAG = 'sale_tag'
+STOCK = 'stock'
+IN_STOCK = 'in_stock'
+COUNT = 'count'
+ASSETS = 'assets'
+MAIN_IMAGE = 'main_image'
+SET_IMAGE = 'set_images'
+VIEW360 = 'view360'
+VIDEO = 'video'
+METADATA = 'metadata'
+VARIANTS = 'variants'
+
+XPATHS_LIST = {
+    MARKETING_TAGS: XPATH_MARKETING_TAG,
+    SECTION: XPATH_SECTION,
+    IN_STOCK: XPATH_IN_STOCK,
+}
+
 XPATHS = {
-    'marketing_tags': XPATH_MARKETING_TAG,
-    'brand': XPATH_BRAND,
-    'section': XPATH_SECTION,
-    'current': XPATH_PRICE_CURRENT,
-    'original': XPATH_PRICE_ORIGINAL,
-    'in_stock': XPATH_IN_STOCK,
-    'main_image': XPATH_MAIN_IMAGE,
-    'description_key': XPATH_DEC_KEY,
-    'description_value': XPATH_DEC_VALUE,
+    BRAND: XPATH_BRAND,
+    CURRENT: XPATH_PRICE_CURRENT,
+    ORIGINAL: XPATH_PRICE_ORIGINAL,
+    MAIN_IMAGE: XPATH_MAIN_IMAGE,
+    DECSRIPTION_KEY: XPATH_DEC_KEY,
+    DECSRIPTION_VALUE: XPATH_DEC_VALUE,
 }
 
 SECTION_ONLY = -1
+# Убирает страницу парсинга "из хлебных крпошек"
 
 
 # utils.py
-RE_TITLE = r'(.*?)(\d+[.,]?\d*?\s*(?:мл|гр))(.*)'
+RE_TITLE = r"(.*?)(\d+[.,]?\d*?\s*(?:мл|гр))(.*)"
 # Регулярное выражение для поиска обьема продукта в строке title
-SPICE = ' '
-EMPTY_STRING = ''
-RE_FLOAT_OR_INT = r'(\d*[.,]\d+)|\d+'
+RE_FLOAT_OR_INT = r"(\d*[.,]\d+)|\d+"
 # Регулярное выражение для поиска дробного или целого числа
--- im_parser/spiders/maksavit.py
+++ im_parser/spiders/maksavit.py
@@ -1,5 +1,7 @@
 import scrapy
-from typing import Dict
+
+from scrapy.http import Request, Response
+from typing import Dict, Generator
 from datetime import datetime as dt
 from im_parser.constants import (
     DOMAIN_NAME,
@@ -10,11 +12,35 @@ from im_parser.constants import (
     XPATH_TITLE,
     SECTION_ONLY,
     XPATHS,
+    XPATHS_LIST,
     EMPTY_STRING,
     XPATH_METADATA,
     XPATH_METADATA_H3,
     XPATH_TEXT,
-    XPATH_SIBLING_TEXT,
+    XPATH_SIB_TEXT,
+    BRAND,
+    ORIGINAL,
+    CURRENT,
+    DECSRIPTION_KEY,
+    DECSRIPTION_VALUE,
+    MARKETING_TAGS,
+    SECTION,
+    TIMESTAMP,
+    RPC,
+    URL,
+    TITLE,
+    PRICE_DATA,
+    SALE_TAG,
+    STOCK,
+    IN_STOCK,
+    COUNT,
+    ASSETS,
+    MAIN_IMAGE,
+    SET_IMAGE,
+    VIEW360,
+    VIDEO,
+    METADATA,
+    VARIANTS,
 )
 from im_parser.utils import (
     title_split_string,
@@ -30,7 +56,7 @@ class MaksavitSpider(scrapy.Spider):
     allowed_domains = [DOMAIN_NAME + RU]
     start_urls = [START_URL]
 
-    def parse(self, response):
+    def parse(self, response: Response) -> Generator[Request, None, None]:
         product_page_urls = response.xpath(XPATH_PAGE_URL).extract()
         for link in product_page_urls:
             yield response.follow(link, self.parse_product)
@@ -39,68 +65,71 @@ class MaksavitSpider(scrapy.Spider):
         self.page_index += 1
         yield response.follow(next_page, callback=self.parse)
 
-    def parse_product(self, response) -> Dict[str, str]:
+    def parse_product(self, response: Response) -> Dict[str, str]:
         product_id = get_number_from_string(response.url)
-        product_title = response.xpath(XPATH_TITLE).extract()[0]
+        product_title = response.xpath(XPATH_TITLE).extract_first()
         title_name, volume_product = title_split_string(product_title)
 
+        extracted_data_list = {
+            key: strip_space(response.xpath(value).extract())
+            for key, value in XPATHS_LIST.items()
+        }
         extracted_data = {
-            key: strip_space(response.xpath(xpath).extract())
-            for key, xpath in XPATHS.items()
+            key: response.xpath(value).extract_first()
+            for key, value in XPATHS.items()
         }
 
-        brand = extracted_data['brand']
-        if brand:
-            brand = brand[0]
+        brand = extracted_data[BRAND]
 
-        original = extracted_data['original']
-        current = extracted_data['current']
+        original = extracted_data[ORIGINAL]
+        current = extracted_data[CURRENT]
         if current:
-            current = float(get_number_from_string(current[0]))
+            current = float(get_number_from_string(current))
         if current and original:
-            original = float(get_number_from_string(original[0]))
+            original = float(get_number_from_string(original))
             sale_tag = discount_percentage_calc(current, original)
 
         metadata = {}
-        description_key = extracted_data.get('description_key')
-        description_value = extracted_data.get('description_value')
+        description_key = extracted_data[DECSRIPTION_KEY]
+        description_value = extracted_data[DECSRIPTION_VALUE]
         if description_key and description_value:
-            metadata[description_key[0]] = description_value[0]
+            metadata[description_key] = description_value
 
             metadata_body = {}
             for div in response.xpath(XPATH_METADATA):
                 for h3 in div.xpath(XPATH_METADATA_H3):
                     key = h3.xpath(XPATH_TEXT).extract_first()
                     value = (
-                        h3.xpath(XPATH_SIBLING_TEXT).extract_first() or ''
+                        h3.xpath(XPATH_SIB_TEXT).extract_first()
+                        or EMPTY_STRING
                     ).strip()
                     metadata_body[key] = value
             metadata.update(metadata_body)
-
+        sale_tag = (f'Скидка {sale_tag}%' if original else EMPTY_STRING,)
         data = {
-            'timestamp': dt.now().timestamp(),
-            'RPC': product_id,
-            'url': response.url,
-            'title': f'{title_name}, {volume_product}',
-            'marketing_tags': extracted_data['marketing_tags'],
-            'brand': brand,
-            'section': extracted_data['section'][:SECTION_ONLY],
-            'price_data': {
-                'current': current,
-                'original': original,
-                'sale_tag': f'Скидка {sale_tag}%' if original else '',
+            TIMESTAMP: dt.now().timestamp(),
+            RPC: product_id,
+            URL: response.url,
+            TITLE: f'{title_name}, {volume_product}',
+            MARKETING_TAGS: extracted_data_list[MARKETING_TAGS],
+            BRAND: brand.strip() if brand else EMPTY_STRING,
+            SECTION: extracted_data_list[SECTION][:SECTION_ONLY],
+            PRICE_DATA: {
+                CURRENT: original if current == original else current,
+                ORIGINAL: original if original else current,
+                SALE_TAG: sale_tag,
             },
-            'stock': {
-                'in_stock': True if extracted_data['in_stock'] else False,
-                'count': 0,
+            STOCK: {
+                IN_STOCK: True if extracted_data_list[IN_STOCK] else False,
+                COUNT: 0,
             },
-            'assets': {
-                'main_image': extracted_data['main_image'],
-                'set_images': [EMPTY_STRING],
-                'view360': [EMPTY_STRING],
-                'video': [EMPTY_STRING],
+            ASSETS: {
+                MAIN_IMAGE: extracted_data[MAIN_IMAGE],
+                SET_IMAGE: [EMPTY_STRING],
+                VIEW360: [EMPTY_STRING],
+                VIDEO: [EMPTY_STRING],
             },
-            'metadata': metadata,
-            'variants': 0,
+            METADATA: metadata,
+            VARIANTS: 0,
         }
         yield data
