a7e80f6f654ba9e47df6492ba7007c04a94fe626
priselkov <priselkov@gardium.ru>
2023-11-11 16:48:25 +0300

    Add test spider

---
+++ im_parser/spiders/proxy_test.py
@@ -0,0 +1,19 @@
+﻿import scrapy
+
+from im_parser.constants import META
+
+
+class ProxyTestSpider(scrapy.Spider):
+    name = 'proxy_test'
+
+    def start_requests(self):
+        '''Паук для проверки работы прокси.'''
+        url = 'http://icanhazip.com'
+        yield scrapy.Request(
+            url, callback=self.parse,
+            meta=META
+        )
+
+    def parse(self, response):
+        '''Пишет в логи ip c которого был запрос.'''
+        self.log('Ответ сервера: %s' % response.text)
